f9c5345c3884a14c023773b938f0f2f6ebcd3bae
FurkanByte <70027460+FurkanByte@users.noreply.github.com>
2024-12-15 17:15:35 +0200

    Structers, Function Prototypes and menu Function added

--- Part1.c
+++ Part1.c
@@ -1,10 +1,58 @@
 //
 // Created by Furkan on 15.12.2024.
 // This file created for EENG212 Lab Project
+// Library Management System with Structers and Arrays
 //
+
 #include <stdio.h>
 #include <stdlib.h>
 
+//Book Structer
+typedef struct {
+    int bookID;
+    char title[100];
+    char author[100];
+    int yearPublished;
+    int isAvailable; // 1 available: 0 not available
+} book;
+
+//User Structer
+typedef struct {
+    int userID;
+    char name[100];
+    int checkedOutBooks[10]; //IDs of the books borrowed by the user.
+} user;
+
+//Function Prototypes
+void addBook();
+void deleteBook();
+int searchBookByTitle();
+int searchBookByAuthor();
+void checkOutBook();
+void returnBook();
+void listBooks();
+void listBorrowedBooks(); //by a user
+int menu();
+
 void main(){
-  printf("Hello World\n");
+    int option = menu();
+
   }
+
+int menu() {
+    int option;
+    printf("Library Managment System\n");
+    printf("--------------------------\n");
+    printf("1. Add New Book\n");
+    printf("2. Delete Book\n");
+    printf("3. Search Book by Title\n");
+    printf("4. Search Book by Author\n");
+    printf("5. Check out Book (to a user)\n");
+    printf("6. Return Book (from a user)\n");
+    printf("7. List All Books\n");
+    printf("8. List Borrowed Books of User\n");
+    printf("9. Exit\n");
+    printf("Enter your option: ");
+    scanf("%d", &option);
+    return option;
+}
