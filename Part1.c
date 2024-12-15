0712f95748b58be23fd1116addd81a504bcbf7c1
FurkanByte <70027460+FurkanByte@users.noreply.github.com>
2024-12-15 16:57:33 +0200

    Files Created

---
+++ Part1.c
@@ -0,0 +1,10 @@
+//
+// Created by Furkan on 15.12.2024.
+// This file created for EENG212 Lab Project
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+  printf("Hello World\n");
+  }
---
+++ Part2.c
@@ -0,0 +1,9 @@
+//
+// Created by furka on 15.12.2024.
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+    printf("Hello World2\n");
+}
