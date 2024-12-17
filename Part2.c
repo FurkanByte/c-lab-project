5e18a69e94451a4a62eb53ecd6b812e038f87e7d
FurkanByte <70027460+FurkanByte@users.noreply.github.com>
2024-12-17 17:37:05 +0200

    Part 2 Started
    - Function prototypes created
    - Structers created

--- Part2.c
+++ Part2.c
@@ -1,8 +1,36 @@
 //
-// Created by furka on 15.12.2024.
+// Created by Furkan on 15.12.2024.
+// This file created for EENG212 Lab Project
+// Library Management System with Linked Lists
 //
+
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct bookNode {
+    int bookID;
+    char title[100];
+    char author[100];
+    int yearPublished;
+    int isAvailable; //1 Available: 0 Not Available
+    struct bookNode *next;
+} bookNode;
+
+typedef struct userNode {
+    int userID;
+    char name[100];
+    struct userNode *next;
+    bookNode* borrowedBooks;
+} userNode;
+
+void addBook(bookNode **head, int bookID, const char* title, const char* author, int yearPublished);
+void deleteBook(bookNode **head, int bookID);
+bookNode* searchBookByTitle(bookNode* head, const char* title);
+bookNode* searchBookByAuthor(bookNode* head, const char* author);
+void checkOutBook(userNode *userHead, int userID, bookNode *bookHead, int bookID);
+void returnBook(userNode *userHead, int userID, bookNode *bookHead, int bookID);
+void listBooks(bookNode *head);
+void listBorrowedBooks(userNode *userHead, int userID);
 
 void main(){
     printf("Hello World2\n");
